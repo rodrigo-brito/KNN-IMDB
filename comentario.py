240175ada26494e4fe5bb72ab460c9223e15ddc6
Rodrigo Brito <rig.10@hotmail.com>
2016-04-19 23:03:12 -0300

    Criação de funções e operadores básicos

--- comentario.py
+++ comentario.py
@@ -1,9 +1,64 @@
+import numpy
+
 class Comentario(object):
+    # Metodo construtor
     def __init__( self, classificacao, texto ):
         self.classificacao = classificacao
         self.texto = texto
         self.palavras = texto.split(" ")
+
+    # Metodo de impressao de objeto
     def __str__( self ):
         return self.classificacao+' - '+self.texto
-    def hasWord( self, palavra ):
-        return palavra in self.palavras
+
+    # Verifica se o comentario possui uma dada plavra
+    def hasWord( self, word ):
+        return word in self.palavras
+
+    def jaccardDistance( self, comentario):
+        # Array de palavras unicas
+        uniqWords  = []
+        # Array a uniao de palavras registradas em ambos comentarios
+        allWords 	= numpy.concatenate([comentario.palavras, self.palavras])
+
+        # Montando lista de palavras unicas
+        for word in allWords:
+            if word not in uniqWords:
+                uniqWords.append(word)
+
+        # Variaveis para calculo de Jaccard
+        f11 = 0.0
+        f10 = 0.0
+        f01 = 0.0
+
+        for word in uniqWords:
+            if self.hasWord( word ) and comentario.hasWord( word ):
+                f11+=1.0
+            elif self.hasWord( word ):
+                f10+=1.0
+            elif comentario.hasWord( word ):
+                f01+=1.0
+        # Calcula a distancia de Jaccard
+        return f11/(f10+f01+f11)
+
+    # Retorna lista de vizinhos mais semelhantes
+    def getNeighbors(self, trainList, size ):
+        neighbors = trainList[:] #Copia vetor para alteracao
+        # Calcula similaridade
+        for comentario in neighbors:
+            comentario.jaccard = self.jaccardDistance( comentario )
+        neighbors.sort(key=lambda comentario: comentario.jaccard, reverse=True)
+        return neighbors[:size]
+
+    # Verifica a classificacao da maioria dos vizinhos
+    def getNeighborsClass( self, trainList, size ):
+        neighbors = self.getNeighbors( trainList, size )
+        count = 0
+        for neighbor in neighbors:
+            if neighbor.classificacao == '1':
+                count+=1
+        if count >= size/2: #se mais da metade dos vizinhos for 1
+            return 1
+        else:
+            return 0
+
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from comentario import Comentario
 
-def lerarquivo( nome_arquivo ):
+# Le arquivo de comentarios e retorna vetor com objetos separados
+def readComments( nome_arquivo ):
     comentarios = []
     arquivo = open( nome_arquivo, 'r' )  # abre o arquivo para leitura
     print( "Lendo arquivo '%s'..." %nome_arquivo )
@@ -12,7 +13,8 @@ def lerarquivo( nome_arquivo ):
     arquivo.close()  # depois do uso, fecha o arquivo
     return comentarios
 
-def lerStopWords( nome_arquivo ):
+# Le arquivo com lista de stopwords
+def readStopWords( nome_arquivo ):
     stopWords = []
     arquivo = open( nome_arquivo, 'r' )  # abre o arquivo para leitura
     print( "Lendo arquivo '%s'..." %nome_arquivo )
@@ -21,11 +23,26 @@ def lerStopWords( nome_arquivo ):
     arquivo.close()  # depois do uso, fecha o arquivo
     return stopWords
 
+# Percorre cada comentario e remove as palavras presentes no stopworlist
+def removeStopWords( dataset, stopword_list ):
+    print "Removendo StopWords..."
+    for data in dataset:
+        for word in data.palavras:
+            if word in stopword_list:
+                data.palavras.remove( word )
+
+
+# Metodo principal
 def main():
-    teste_inicial = lerarquivo( "files/teste_inicial.txt" )
-    imdb_test = lerarquivo( "files/imdb_test" )
-    imdb_train = lerarquivo( "files/imdb_train" )
-    stopword_list = lerStopWords( "files/stopword_list" )
-    for palavra in teste_inicial:
-        print palavra, ' = ', palavra.hasWord( 'brito' )
+    teste_inicial = readComments( "files/teste_inicial.txt" )
+    imdb_test = readComments( "files/imdb_test" )
+    imdb_train = readComments( "files/imdb_train" )
+    stopword_list = readStopWords( "files/stopword_list" )
+
+    removeStopWords( teste_inicial, stopword_list )
+    for c in teste_inicial:
+        print c.palavras
+
+    print "KNN = ",imdb_test[0].getNeighborsClass(imdb_train[:100], 100),
+    print " REAL = ",imdb_test[0].classificacao
 main()
